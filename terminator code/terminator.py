5819f0d01b680d0c65cf8420cd3a7db2a9b1b952
justinsly <134002548+justinsly@users.noreply.github.com>
2024-03-30 21:27:51 +0000

    Update terminator.py
    added a new process to terminate. will be commented out until i figure out how to run the code as administrator, as windows denies access

--- terminator code/terminator.py
+++ terminator code/terminator.py
@@ -1,20 +1,24 @@
 import os
 
-print("getting rid of junk/useless processes...")
+print("getting rid of junk processes...")
 
 os.system("taskkill /f /im PhoneExperienceHost.exe") #who uses phone link?
 os.system("taskkill /f /im CompatTelRunner.exe") #no good use and might hog your drive if you're using an hdd
 os.system("taskkill /f /im smartscreen.exe") #if you're reading this then you are smart enough to not need smartscreen
 #os.system("net stop wuauserv")
-#^ doesnt work
+#^ helf until further notice
 
 
 print("=========================================================================")
 
-killff = input("press y then enter to close all firefox open and background tasks, press anything else to keep: ")
+killff = input("press y then enter to close all open and background firefox tasks, press anything else to keep: ")
 
 if killff == "y":
     os.system("taskkill /f /im  firefox.exe")
 
+#killspool = input("press y to close spooler subsystem app (used to print stuff), press any other key to keep: ")
 
+#if killspool == "y":
+    #os.system("taskkill /f /im spoolsv.exe")
+#^ held until further notice
 input("Finished. Press Enter to quit")
