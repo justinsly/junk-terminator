219d259d9facfac34dba6548ceba9c9f11f6dd0f
justinsly <134002548+justinsly@users.noreply.github.com>
2024-03-31 13:08:42 +0000

    Update terminator.py
    new (admin-only) process + added new comments

--- terminator code/terminator.py
+++ terminator code/terminator.py
@@ -4,10 +4,13 @@ print("getting rid of junk processes...")
 
 os.system("taskkill /f /im PhoneExperienceHost.exe") #who uses phone link?
 os.system("taskkill /f /im CompatTelRunner.exe") #no good use and might hog your drive if you're using an hdd
+#^ seems to not work due to the script not having admin access, wont comment out tho
 os.system("taskkill /f /im smartscreen.exe") #if you're reading this then you are smart enough to not need smartscreen
+os.system("taskkill /f /im TiWorker.exe") #the process doesnt seem to do anything useful
+#^ also requires admin access
 #os.system("net stop wuauserv")
+#os.system("net stop SysMain")
 #^ held until further notice
-
 
 print("=========================================================================")
 
