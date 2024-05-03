0943b2808055c64123b21292a60107b8886a1789
justinsly <134002548+justinsly@users.noreply.github.com>
2024-05-03 16:54:25 +0100

    added a comment

--- terminator code/runterm.bat
+++ terminator code/runterm.bat
@@ -6,6 +6,8 @@ ECHO starting terminator...
 
 ECHO ===========================
 
+:: this single command shall not be touched
+:: in any condition since its required to run as admin
 CD %~dp0
 
 PYTHON terminator.py
