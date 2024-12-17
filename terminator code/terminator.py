07f2e77e4fde5afe32ed0b30dbd855e3636fcc06
justinsly <134002548+justinsly@users.noreply.github.com>
2024-12-17 17:11:16 +0100

    removed a stupid useless tip

--- terminator code/terminator.py
+++ terminator code/terminator.py
@@ -4,7 +4,7 @@ import subprocess
 
 os.system("title Junk Terminator")
 
-tips = ["cool tip: its recommended to periodically run this program every time", "cool tip: you can contribute to the program on https://github.com/justinsly/junk-terminator", "cool tip: you can", "cool tip: give me all your money (pls)"]
+tips = ["cool tip: its recommended to periodically run this program every time", "cool tip: you can", "cool tip: give me all your money (pls)"]
 
 print("getting rid of junk processes...")
 
